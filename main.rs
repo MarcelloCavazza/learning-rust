037f8e2b9765fefd5d023468c752b95ef555ae64
MarcelloCavazza <marcellocavazzaoliveira@gmail.com>
2022-12-03 01:33:37 -0300

    creating first code2.0

--- main.rs
+++ main.rs
@@ -1,6 +1,6 @@
 //after creating the code, run rustc main.rs, to compile
 //that will create the project in a portable and executable file
-
+//then execute the file and see the magic
 fn main() {
     println!("Hello world2");
 }
